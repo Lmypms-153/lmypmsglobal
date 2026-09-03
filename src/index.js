55dff5f1f9708bdf540937e74b1052675363e235
Lmypms-153 <midmart24uk@gmail.com>
2026-09-03 17:42:52 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-async function loadApp() {
-  const { default: App } = await import('../App')
-  const root = ReactDOM.createRoot(document.getElementById('root'))
-  root.render(<React.StrictMode><App /></React.StrictMode>)
-}
-
-loadApp()
+import App from '../App'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
